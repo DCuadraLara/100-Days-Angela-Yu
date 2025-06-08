8d3a5cfb25c16a8e20cb01963890f5141a88b165
DCuadraLara <93824412+DCuadraLara@users.noreply.github.com>
2025-06-08 12:44:23 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -16,8 +16,6 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
-reset = ""
-
 calculator_dict = {
     "+": add,
     "-": subtract,
@@ -25,30 +23,26 @@ calculator_dict = {
     "/": divide
 }
 while True:
-    while reset == "y":
+    n1 = float(input("What's the first number?: "))
+
+    while True:
+        print("Available operations: ")
         for key in calculator_dict:
             print(key)
-        operation = input("Pick an operation ")
-        n2 = int(input("What's the second number?: "))
+
+        operation = input("Pick an operation: ")
+        n2 = float(input("What's the second number?: "))
 
         operation_result = calculator_dict[operation](n1, n2)
-        print(operation_result)
+        print(f"Result: {operation_result}")
 
-        reset = input("Would you like to continue or go into a new operation? Y/N").lower()
+        reset = input("Would you like to continue with the result? ").lower()
         if reset == "y":
             n1 = operation_result
-        if reset == "n":
+        else:
             break
 
-    n1 = int(input("What's the first number?: "))
-    for key in calculator_dict:
-        print(key)
-    operation = input("Pick an operation ")
-    n2 = int(input("What's the second number?: "))
-
-    operation_result= calculator_dict[operation](n1, n2)
-    print(operation_result)
-
-    reset = input("Would you like to continue or go into a new operation? Y/N" ).lower()
-    if reset == "y":
-        n1 = operation_result
+    reset = input("Would you like to continue or go into a new operation? Y/N").lower()
+    if reset != "y":
+        print("Bye Bye!")
+        break
