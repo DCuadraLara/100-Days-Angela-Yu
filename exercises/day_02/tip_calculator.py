7acf62f3572dd0ee64617035f6b9120fdefc8b06
DCuadraLara <93824412+DCuadraLara@users.noreply.github.com>
2025-09-17 16:23:03 +0200

    Update tip_calculator.py

--- exercises/day_02/tip_calculator.py
+++ exercises/day_02/tip_calculator.py
@@ -1,7 +1,9 @@
+"""Basic example to calculate the tip we should pay"""
+
+# Variables
+bill = 150.00
+
 print("Welcome to the tip calculator!")
-
-people_num = int(input("Number of people: "))
-total_bill = 150.00
-split_bill = (total_bill / people_num) * 1.12
-
-print(f"{split_bill:.2f}")
+people = int(input("Number of people: "))
+split_bill = (bill / people) * 1.12
+print(f"Each person should pay:{split_bill:.2f} Dollars") # split_bill:2f shows 2 decimates.
