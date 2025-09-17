ae5ffb29b5d05fa6bd13dc1e349f21546b7b1e80
DCuadraLara <93824412+DCuadraLara@users.noreply.github.com>
2025-09-17 16:12:10 +0200

    Update tip_calculator.py

--- exercises/day_02/tip_calculator.py
+++ exercises/day_02/tip_calculator.py
@@ -1,1 +1,7 @@
+print("Welcome to the tip calculator!")
 
+people_num = int(input("Number of people: "))
+total_bill = 150.00
+split_bill = (total_bill / people_num) * 1.12
+
+print(f"{split_bill:.2f}")
