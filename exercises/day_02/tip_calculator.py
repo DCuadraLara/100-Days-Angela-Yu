4bd41e230552c50481997119e1689a90d23fed07
DCuadraLara <93824412+DCuadraLara@users.noreply.github.com>
2025-10-15 11:58:56 +0200

    Update tip_calculator.py

--- exercises/day_02/tip_calculator.py
+++ exercises/day_02/tip_calculator.py
@@ -7,4 +7,4 @@ print("Welcome to the tip calculator!")
 people = int(input("Number of people: "))
 split_bill = (bill / people) * 1.12
 
-print(f"Each person should pay:{split_bill:.2f} Dollars") # split_bill:2f shows 2 decimates.
+print(f"Each person should pay:{split_bill:.2f} Dollars")  # split_bill:2f shows 2 decimates.
